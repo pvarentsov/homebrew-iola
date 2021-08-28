7454b92ba0211c45da380912d58ba8501e2818fc
Pavel Varentsov <varentsovpavel@gmail.com>
2021-08-29 01:59:15 +0300

    0.4.2

--- Formula/iola.rb
+++ Formula/iola.rb
@@ -4,8 +4,8 @@
 class Iola < Formula
   desc "Socket client with REST API"
   homepage "https://github.com/pvarentsov/iola"
-  url "https://github.com/pvarentsov/iola/releases/download/v0.4.2/TODO.tar.gz"
-  sha256 "TODO hash"
+  url "https://github.com/pvarentsov/iola/releases/download/v0.4.2/iola-macos-amd64.tar.gz"
+  sha256 "bc8674612b0887df59136b2f728441d724780fe0304b7474da28d6768d2bc04d"
   version "0.4.2"
 
   def install
