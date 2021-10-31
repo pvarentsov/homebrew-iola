bb6c849b7b52c30d2b01003102b99c706e4979cf
Pavel Varentsov <varentsovpavel@gmail.com>
2021-10-31 23:44:19 +0300

    v0.5.5

--- Formula/iola.rb
+++ Formula/iola.rb
@@ -5,16 +5,16 @@ class Iola < Formula
   desc "Socket client with REST API"
   homepage "https://github.com/pvarentsov/iola"
   license "MIT"
-  version "0.5.4"
+  version "0.5.5"
 
   on_linux do
-    url "https://github.com/pvarentsov/iola/releases/download/v0.5.4/iola-v0.5.4-linux-amd64.zip"
-    sha256 "6579008e6a0b72c4ea85c4e6e88f7f63a96fee4dfc89d2f204e72e60f3eb5744"
+    url "https://github.com/pvarentsov/iola/releases/download/v0.5.5/iola-v0.5.5-linux-amd64.zip"
+    sha256 "9815a17d67f44901733584db10e7053a0bf2af4442a267a92eb9737c8247cfa6"
   end
 
   on_macos do
-    url "https://github.com/pvarentsov/iola/releases/download/v0.5.4/iola-v0.5.4-macos-amd64.zip"
-    sha256 "dcd4d458c5e1b599a4e66997904e51838abe3217b09806fffe0b2b29f924510f"
+    url "https://github.com/pvarentsov/iola/releases/download/v0.5.5/iola-v0.5.5-macos-amd64.zip"
+    sha256 "cfe9d0a5f9489c0c3a63a4a4a83a402f5c83cd88ac16f37165a24075c7b6d379"
   end
 
   conflicts_with "iola"
