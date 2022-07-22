fa787b9d052649d1cffd2c3685f1c6bc2e3bb4db
pvarentsov <varentsovpavel@gmail.com>
2022-07-23 00:33:36 +0300

    Update formula

--- Formula/iola.rb
+++ Formula/iola.rb
@@ -7,14 +7,19 @@ class Iola < Formula
   license "MIT"
   version "0.5.7"
 
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/pvarentsov/iola/releases/download/v0.5.7/iola-v0.5.7-macos-amd64.zip"
+    sha256 "75e97e26e2937196f368ce2ab0b873182c8c683c613a34a60cd2ff2fad36165a"
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/pvarentsov/iola/releases/download/v0.5.7/iola-v0.5.7-macos-amd64.zip"
+    sha256 "75e97e26e2937196f368ce2ab0b873182c8c683c613a34a60cd2ff2fad36165a"
+  end
+
   on_linux do
     url "https://github.com/pvarentsov/iola/releases/download/v0.5.7/iola-v0.5.7-linux-amd64.zip"
     sha256 "06d91835fe64ce097e152ec95ddfbbff1fe8aacb40e96c7ffb5ee7088cd96f92"
-  end
-
-  on_macos do
-    url "https://github.com/pvarentsov/iola/releases/download/v0.5.7/iola-v0.5.7-macos-amd64.zip"
-    sha256 "75e97e26e2937196f368ce2ab0b873182c8c683c613a34a60cd2ff2fad36165a"
   end
 
   conflicts_with "iola"
